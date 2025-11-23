5726c21d372933807abd50a2e20b9e9cfaccb8a4
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2025-11-22 22:19:16 -0500

    remove sst

--- packages/auth/src/client.ts
+++ packages/auth/src/client.ts
@@ -1,8 +1,8 @@
+import { env } from '@gaming/zod';
 import { adminClient, usernameClient } from 'better-auth/client/plugins';
 import { createAuthClient } from 'better-auth/react';
-import { Resource } from 'sst';
 
 export const authClient = createAuthClient({
   plugins: [usernameClient(), adminClient()],
-  baseURL: env.VITE_AUTH_URL,
+  baseURL: env.VITE_APP_URL,
 });
